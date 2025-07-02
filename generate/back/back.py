7124f96431b36db49bcd970f9821542a29dce524
tripleS-Dev <155389247+tripleS-Dev@users.noreply.github.com>
2025-07-02 23:27:17 +0900

    Add support for custom back images and logos, enhance sidebar handling

--- generate/back/back.py
+++ generate/back/back.py
@@ -5,11 +5,22 @@ from utils import color_change, get_json, paste_correctly, text_draw
 
 BASE_DIR = str(Path(__file__).resolve().parent) + '/resources/'
 
-def generate_back(json) -> Image.Image:
+def generate_back(json, back_img=None, side_logo=None, top_logo=None) -> Image.Image:
+
     backside, layout, inside = open_images()
 
-    inside = color_change(inside, get_json(json, 'appearance.background_color', '#000000'))
+
+    if back_img:
+        inside = back_img
+    else:
+        inside = color_change(inside, get_json(json, 'appearance.background_color', '#000000'))
+
+
     layout = color_change(layout, get_json(json, 'appearance.text_color', '#000000'))
+
+    if top_logo:
+        top_logo = color_change(top_logo, get_json(json, 'appearance.text_color', '#000000'))
+        backside = paste_correctly(backside, (57, 151), top_logo)
 
 
     backside = paste_correctly(backside, (0,0), inside)
@@ -23,6 +34,25 @@ def generate_back(json) -> Image.Image:
     text_draw(draw, (55, 471), 'Helvetica_Neue_LT_Std_65_Medium-4.otf', 126, get_json(json, 'artist.name', ''), get_json(json, 'appearance.text_color', '#000000'))
     text_draw(draw, (55, 471+216), 'Helvetica_Neue_LT_Std_65_Medium-4.otf', 126, get_json(json, 'text_area.class', ''), get_json(json, 'appearance.text_color', '#000000'))
     text_draw(draw, (55, 471+216+216), 'Helvetica_Neue_LT_Std_65_Medium-4.otf', 126, get_json(json, 'text_area.season', ''), get_json(json, 'appearance.text_color', '#000000'))
+
+
+
+    sidebar = Image.open(BASE_DIR + 'sidebar.png')
+
+    if side_logo:
+        side_logo = color_change(side_logo, get_json(json, 'appearance.text_color', '#000000'))
+        sidebar = paste_correctly(sidebar, (1478 - 55 - side_logo.size[0], 37), side_logo)
+
+    else:
+        draw = ImageDraw.Draw(sidebar)
+        text_draw(draw, (1426, 45), 'Helvetica_Neue_LT_Std_75_Bold.otf', 58, get_json(json, 'artist.group', ''),
+                  get_json(json, 'appearance.text_color', '#000000'), pos=2)
+
+    sidebar = sidebar.rotate(270, expand=True)
+    backside = paste_correctly(backside, (865, 98), sidebar)
+
+
+
 
     return backside
 
