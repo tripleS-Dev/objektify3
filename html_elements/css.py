35346fa17ac59fef7752d3ef79ad87c752956486
tripleS-Dev <155389247+tripleS-Dev@users.noreply.github.com>
2025-07-11 04:46:24 +0900

    Update CSS media queries for responsive layout and adjust sticky image height parameters

--- html_elements/css.py
+++ html_elements/css.py
@@ -13,10 +13,22 @@ body, html, .gradio-container, .gradio-container > .gr-block {
     height: 45dvh;
     z-index: 1000;
 }
+
+@media (min-width: 400px) and (max-width: 555px) {
+    .sticky-image {
+        margin-left: 4dvw;
+        margin-right: 4dvw;
+    }
+}
+@media (min-aspect-ratio: 0.8/1) and (max-aspect-ratio: 1.3/1) {
+    .sticky-image {
+        height: 60dvh;
+    }
+}
 /* When dvh < dvw, set height to 80dvh */
 @media (min-aspect-ratio: 1.3/1) {
     .sticky-image {
-        height: 80dvh;
+        height: 85dvh;
     }
 }
 
