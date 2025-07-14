2fd8d43374e7a71df274d3a1bd10ef793c6234a0
tripleS-Dev <155389247+tripleS-Dev@users.noreply.github.com>
2025-07-15 01:00:21 +0900

    Add Name, Class, Season, and logo fields; reorganize advanced UI layout

--- components/advanced.py
+++ components/advanced.py
@@ -15,23 +15,9 @@ def advanced():
 
 
             with gr.Group():
-                with gr.Row(equal_height=True):
-                    group_type = gr.Radio(choices=['Text', 'Image'], label='Group Type', value='Text')
-                    group_text = gr.Text(label='Group')
-
-                group_image = gr.Image(label='Group Image', visible=False, interactive=True)
-                group_type.change(fn=lambda x: [gr.Text(visible=True), gr.Text(visible=False)] if x == 'Text' else [gr.Text(visible=False), gr.Text(visible=True)], inputs=group_type, outputs=[group_text, group_image])
-
-            with gr.Group():
-                with gr.Row():
-                    number = gr.Textbox(label='Number', value='', interactive=True)
-                    alphabet = gr.Textbox(label='Alphabet', value='', interactive=True)
-                    serial = gr.Textbox(label='Serial', value='', interactive=True)
-
-
-            with gr.Group():
                 with gr.Row():
                     name = gr.Text(label='Name')
+                    group_text = gr.Text(label='Group')
 
                 with gr.Row():
                     class_ = gr.Text(label='Class')
@@ -40,27 +26,36 @@ def advanced():
                     season = gr.Text(label='Season')
                     season_outline = gr.Text(label='Season outline')
 
-
             with gr.Group():
-                with gr.Column():
+                with gr.Row():
+                    number = gr.Textbox(label='Number', value='', interactive=True)
+                    alphabet = gr.Textbox(label='Alphabet', value='', interactive=True)
+                    serial = gr.Textbox(label='Serial', value='', interactive=True)
+
+            with gr.Row():
+                with gr.Group():
+                    sign = gr.Image(label='Sign', sources='upload')
                     sign_scale = gr.Slider(label='Sign Scale', value=50, minimum=0, maximum=100, step=1, interactive=True)
-                    sign = gr.Image(label='Sign')
 
 
 
 
-            with gr.Group():
-                with gr.Column():
+                with gr.Group():
                     qr_code = gr.Text(label='QR Code')
+
+                    qr_logo = gr.Image(label='QR Logo', sources='upload')
                     qr_caption = gr.Text(label='QR Caption')
 
-                with gr.Column():
-                    qr_logo = gr.Image(label='Sign Logo')
+            with gr.Accordion(label='Group Logos', open=False):
+                with gr.Row():
+                    sidebar_logo = gr.Image(label='Sidebar logo', sources='upload')
+                    top_logo = gr.Image(label='Top Logo', sources='upload')
+
 
 
             with gr.Accordion(label='Layouts', open=False):
                 with gr.Row():
-                    sidebar = gr.Image()
-                    back_layout = gr.Image()
+                    sidebar = gr.Image(label='Sidebar', sources='upload')
+                    back_layout = gr.Image(label='Back Layout', sources='upload')
 
     return advanced
