39b6c78651543fcbac65d0e8e8ac79fcf9e3abd3
tripleS-Dev <155389247+tripleS-Dev@users.noreply.github.com>
2026-01-28 15:09:07 +0900

    Remove debug print

--- utils/simple2advanced.py
+++ utils/simple2advanced.py
@@ -6,7 +6,7 @@ BASE_DIR = str(Path(__file__).resolve().parent.parent)
 
 def simple2advanced(data, sign=None, sign_position_x=None, sign_position_y=None, qr_logo=None, top_logo=None, sidebar_logo=None, front_layout = None, back_layout = None):
     #advanced_components = [main_color, text_color, name, group_text, class_, season, season_outline, number, alphabet, serial, sign, sign_scale, sign_position, qr_code, qr_logo, qr_caption, sidebar_logo, top_logo, sidebar, back_layout]
-    print(data)
+    #print(data)
 
     main_color = get_json(data, f'appearance.background_color', '')
 
