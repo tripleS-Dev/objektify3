ab185eac59abe1a3ca392b0ddb2d44ee56b0287a
tripleS-Dev <155389247+tripleS-Dev@users.noreply.github.com>
2026-01-29 16:28:59 +0900

    Add Google AdSense script to no_zoom_head

--- html_elements/no_zoom_head.py
+++ html_elements/no_zoom_head.py
@@ -1,4 +1,6 @@
 no_zoom_head = """
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7105159220925969"
+     crossorigin="anonymous"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta name="google-adsense-account" content="ca-pub-7105159220925969">
 """
