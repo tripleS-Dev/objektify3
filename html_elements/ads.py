62249a0711199524ad18343333b85647805b493a
tripleS-Dev <155389247+tripleS-Dev@users.noreply.github.com>
2026-01-29 17:14:40 +0900

    Add ads component to front page

---
+++ html_elements/ads.py
@@ -0,0 +1,12 @@
+ads = """<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7105159220925969"
+     crossorigin="anonymous"></script>
+<!-- 수평 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7105159220925969"
+     data-ad-slot="6609922481"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>"""
