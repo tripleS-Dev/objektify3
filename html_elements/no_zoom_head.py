b1c8c72e1a78c379df569df7ac9bffbfa967c547
tripleS-Dev <155389247+tripleS-Dev@users.noreply.github.com>
2026-01-29 11:09:50 +0900

    Add Google AdSense meta tag

--- html_elements/no_zoom_head.py
+++ html_elements/no_zoom_head.py
@@ -1,3 +1,4 @@
 no_zoom_head = """
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="google-adsense-account" content="ca-pub-7105159220925969">
 """
